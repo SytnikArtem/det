466be67b7c14fb7a440792efb6907cfda4232e94
Sytnik Artem <artemasitnik@gmail.com>
2018-12-10 19:45:27 +0200

    1
    1

--- app/js/common.js
+++ app/js/common.js
@@ -101,7 +101,7 @@ $(document).ready(function() {
         $(this).parent().siblings().find(".about-menu-link").removeClass('active');
         var elementClick = $(this).attr("href");
         var destination = $(elementClick).offset().top;
-        $('html, body').animate({ scrollTop: destination }, 400);
+        $('html, body').animate({ scrollTop: destination - 100 }, 400);
         positionLine();
         return false;
     });
